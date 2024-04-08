66fd0f1e28bc7c97517db1faae81d527caddd107
unknown <shoxruuh07tuxtanazarov@gmail.com>
2024-04-08 15:40:49 +0500

    btn

--- src/admin/Ustozlar/Ustozlar.vue
+++ src/admin/Ustozlar/Ustozlar.vue
@@ -63,18 +63,20 @@
                                 </h3>
                             </td>
                             <td>
-                                <button @click="deleteTeacher(i.id)">
+                                <button class="delete-btn" @click="deleteTeacher(i.id)">
                                     <svg class="delate" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
                                         viewBox="0 0 24 24">
                                         <path fill="currentColor"
                                             d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z" />
                                     </svg>
                                 </button>
-                                <svg @click="openModalChange" class="change" xmlns="http://www.w3.org/2000/svg"
+                                <button class="change-btn" @click="openModalChange">
+                                    <svg  class="change" xmlns="http://www.w3.org/2000/svg"
                                     width="1em" height="1em" viewBox="0 0 24 24">
                                     <path fill="currentColor"
                                         d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z" />
                                 </svg>
+                                </button>
                             </td>
                         </tr>
                     </tbody>
@@ -100,7 +102,7 @@
                         2
                     </span>
                 </div>
-                <button>
+                <button >
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16">
                         <path fill="currentColor"
                             d="M8.22 2.97a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.751.751 0 0 1-1.042-.018a.751.751 0 0 1-.018-1.042l2.97-2.97H3.75a.75.75 0 0 1 0-1.5h7.44L8.22 4.03a.75.75 0 0 1 0-1.06" />
